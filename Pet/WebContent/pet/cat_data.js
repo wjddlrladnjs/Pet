6db2c545108d9926128f5baad1c86ac8fd915cf3
byJune <wjdtjfgid@gmail.com>
2017-07-26 17:34:36 +0900

    mammal commit

--- Pet/WebContent/pet/cat_data.js
+++ Pet/WebContent/pet/cat_data.js
@@ -4,83 +4,34 @@
 
 // 생성자 생성
 function Cat(kind, fursize, furcolor, hungry, eyecolor, age, sex, health, name, character ) {
-	
-	this.kind = kind;
-	this.fursize = fursize;
-	this.furcolor = furcolor;
-	this.hungry = hungry;
-	this.eyecolor = eyecolor;
-	this.age = age;
-	this.sex = sex;
-	this.health = health;
-	this.name = name;
-	this.character = character;
-	
+
+	Mammal.call(this, kind, furcolor, hungry, eyecolor, age, sex, health, name, character )
 }
 
-Cat.prototype.setKind = function(kind) { this.kind = kind; };
-Cat.prototype.getKind = function() { return this.kind; };
+Cat.prototype = new Mammal();
 
 Cat.prototype.setFursize = function(fursize) { this.fursize = fursize; };
 Cat.prototype.getFursize = function() { return this.fursize; };
 
-Cat.prototype.setFurcolor = function(furcolor) { this.furcolor = furcolor; };
-Cat.prototype.getFurcolor = function () { return this.furcolor; };
-
-Cat.prototype.setHungry = function(hungry) { this.hungry = hungry; };
-Cat.prototype.getHungry = function() { return this.hungry; };
-
-Cat.prototype.setEyecolor = function(eyecolor) { this.eyecolor = eyecolor; };
-Cat.prototype.getEyecolor = function() { return this.eyecolor; };
-
-Cat.prototype.setAge = function(age) { this.age = age; };
-Cat.prototype.getAge = function() { return this.age; };
-
-Cat.prototype.setSex = function(sex) { this.sex = sex; };
-Cat.prototype.getSex = function() { return this.sex; };
-
-Cat.prototype.setHealth = function(health) { this.health = health; };
-Cat.prototype.getHealth = function() { return this.health; };
-
-Cat.prototype.setName = function(name) { this.name = name; };
-Cat.prototype.getName = function () { return this.name; };
-
-Cat.prototype.setCharacter = function(character) { this.character = character; };
-Cat.prototype.getCharacter = function() { return this.character; };
-
-
-Cat.prototype.sleep = function() {
-	this.health += 10;
-	this.hungry -= 5;
-	return ' 잠을 잡니다.';
-}
-Cat.prototype.eat = function() {
-	this.hungry -= 10;
-	return ' 밥을 먹습니다.';
-}
-Cat.prototype.dump = function() {
-	this.hungry += 10;
-	return ' 볼일을 봅니다.';
-}
-Cat.prototype.play = function() {
-	this.hungry -= 10;
-	this.health += 10;
-	return ' 놉니다.'
-}
 Cat.prototype.talk = function() {
 	this.hungry += 10;
 	this.health -= 10;
-	return ' 말을 합니다.';
+	return ' 고양 고양 말을 합니다.';
 }
-Cat.prototype.info = function() {
-	return	'이름 : ' + this.getName() + 
-			', 품종 : ' + this.getKind() + 
-			', 털길이 : ' + this.getFursize() +
-			', 털색: ' + this.getFurcolor() + 
-			', 배고픔: ' + this.getHungry() +
-			', 눈색 : ' + this.getEyecolor() +
-			', 나이 : ' + this.getAge() +
-			', 성별 : ' + this.getSex() +
-			', 건강 : ' + this.getHealth() +
-			', 성격 : ' + this.getCharacter()
+Cat.prototype.eat = function() {
+	this.hungry -= 10;
+	return ' 참치를 먹습니다.';
+}
+
+Cat.prototype.toString = function() {
+	return	'이름 : ' + this.name + 
+			', 품종 : ' + this.kind + 
+			', 털길이: ' + this.fursize +
+			', 털색: ' + this.furcolor + 
+			', 배고픔: ' + this.hungry +
+			', 눈색 : ' + this.eyecolor +
+			', 나이 : ' + this.age +
+			', 성별 : ' + this.sex +
+			', 건강 : ' + this.health +
+			', 성격 : ' + this.character
 }
---
+++ Pet/WebContent/pet/mammal_data.js
@@ -0,0 +1,82 @@
+/**
+ * 		포유류 객체 js
+ */
+
+// 포유류 생성자 생성
+function Mammal( kind, fursize, furcolor, hungry, eyecolor, age, sex, health, name, character ) {
+	
+	this.kind = kind;
+	this.fursize = fursize;
+	this.furcolor = furcolor;
+	this.hungry = hungry;
+	this.eyecolor = eyecolor;
+	this.age = age;
+	this.sex = sex;
+	this.health = health;
+	this.name = name;
+	this.character = character;
+	
+}
+
+Mammal.prototype.setKind = function(kind) { this.kind = kind; };
+Mammal.prototype.getKind = function() { return this.kind; };
+
+Mammal.prototype.setFurcolor = function(furcolor) { this.furcolor = furcolor; };
+Mammal.prototype.getFurcolor = function () { return this.furcolor; };
+
+Mammal.prototype.setHungry = function(hungry) { this.hungry = hungry; };
+Mammal.prototype.getHungry = function() { return this.hungry; };
+
+Mammal.prototype.setEyecolor = function(eyecolor) { this.eyecolor = eyecolor; };
+Mammal.prototype.getEyecolor = function() { return this.eyecolor; };
+
+Mammal.prototype.setAge = function(age) { this.age = age; };
+Mammal.prototype.getAge = function() { return this.age; };
+
+Mammal.prototype.setSex = function(sex) { this.sex = sex; };
+Mammal.prototype.getSex = function() { return this.sex; };
+
+Mammal.prototype.setHealth = function(health) { this.health = health; };
+Mammal.prototype.getHealth = function() { return this.health; };
+
+Mammal.prototype.setName = function(name) { this.name = name; };
+Mammal.prototype.getName = function () { return this.name; };
+
+Mammal.prototype.setCharacter = function(character) { this.character = character; };
+Mammal.prototype.getCharacter = function() { return this.character; };
+
+Mammal.prototype.sleep = function() {
+	this.health += 10;
+	this.hungry -= 5;
+	return ' 잠을 잡니다.';
+}
+Mammal.prototype.eat = function() {
+	this.hungry -= 10;
+	return ' 밥을 먹습니다.';
+}
+Mammal.prototype.dump = function() {
+	this.hungry += 10;
+	return ' 볼일을 봅니다.';
+}
+Mammal.prototype.play = function() {
+	this.hungry -= 10;
+	this.health += 10;
+	return ' 놉니다.'
+}
+Mammal.prototype.talk = function() {
+	this.hungry += 10;
+	this.health -= 10;
+	return ' 말을 합니다.';
+}
+Mammal.prototype.toString = function() {
+	return	'이름 : ' + this.getName() + 
+			', 품종 : ' + this.getKind() + 
+			', 털길이 : ' + this.getFursize() +
+			', 털색: ' + this.getFurcolor() + 
+			', 배고픔: ' + this.getHungry() +
+			', 눈색 : ' + this.getEyecolor() +
+			', 나이 : ' + this.getAge() +
+			', 성별 : ' + this.getSex() +
+			', 건강 : ' + this.getHealth() +
+			', 성격 : ' + this.getCharacter()
+};
